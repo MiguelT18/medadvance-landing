e62a9f575fa8ea9d2da735b8caa152071ed0087b
Miguel Terán <miguel.teranj02@gmail.com>
2026-01-06 01:12:15 -0400

    Add Astro Icon integration and update index page structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    icon({
+      include: {
+        mdi: ["user-circle", "register", "star"],
+        "fluent-color": ["lightbulb-checkmark-32"],
+      },
+    }),
+  ],
+});
